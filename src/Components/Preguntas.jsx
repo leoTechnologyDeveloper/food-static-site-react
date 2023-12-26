7a6cec894691c1f6e81edc933952948c9f798a5d
leoTechnologyDeveloper <www.leotechnology@gmail.com>
2023-12-26 02:13:49 -0500

    Acordion Questions

--- src/Components/Preguntas.jsx
+++ src/Components/Preguntas.jsx
@@ -1,8 +1,45 @@
+import { useState } from "react";
+import Accordion from "./Accordion";
+
 const Preguntas = () => {
+  const [list, setList] = useState([
+    {
+      question: "Cuales son los métodos de pago ?",
+      answer: "Efectivo, Nequi, Transferencia",
+      active: 1,
+    },
+    {
+      question: "Cómo compro los productos ?",
+      answer: "Contacto en nuestra página y vía WhatsApp",
+    },
+    {
+      question: "Cómo es la forma de entrega ?",
+      answer: "Immediata de acuerdo a la distancia",
+    },
+    {
+      question: "Hacen devoluciones ?",
+      answer: "Siempre y cuando este en el tiempo de garantía",
+    },
+  ]);
+
   return (
+    // <div className="w-10/12 flex-col  mx-auto">
+    //   <h2 className="text-3xl my-8">Preguntas Frecuentes </h2>
+    //   <div className="flex flex-col md:flex-row  mx-auto justify-around"></div>
+    // </div>
+
     <div className="w-10/12 flex-col  mx-auto">
-      <h2 className="text-3xl my-8">Preguntas Frecuentes </h2>
-      <div className="flex flex-col md:flex-row  mx-auto justify-around"></div>
+      <h2 className="text-2xl md:text-3xl my-8 font-bold">
+        Preguntas Frecuentes{" "}
+      </h2>
+      {/* <div className="flex flex-col md:flex-row  mx-auto justify-around"></div> */}
+      <div className=" flex justify-center items-center py-5  max-w-screen-lg">
+        <div className="list w-11/12 md:w-3/5">
+          {list.map((item, key) => (
+            <Accordion key={key} datas={item} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
